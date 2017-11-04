7adc65431ec0d5ef42cf4b448a26aca7923f4d19
Administrator <yadoch@gmail.com>
2017-11-04 12:42:45 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/abc/myservice01/MainActivity.java
@@ -0,0 +1,16 @@
+package tw.com.abc.myservice01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/tw/com/abc/myservice01/MyService.java
@@ -0,0 +1,55 @@
+package tw.com.abc.myservice01;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.util.Log;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+    private Timer timer;
+    private int n;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        timer = new Timer();
+        timer.schedule(new MyTask(),1000,0);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+        n=intent.getIntExtra("n",n);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(timer != null){
+            timer.cancel();
+            timer.cancel();
+            timer.purge();
+        }
+    }
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+       return null;
+    }
+    private class MyTask extends TimerTask{
+
+        @Override
+        public void run() {
+            int rand = (int)(Math.random()*n);
+            Log.i("geoff",);
+        }
+    }
+}
